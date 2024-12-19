5380254081243dde56b7e1515dd1f1c0ec9332b7
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-12-19 17:07:45 +0500

    add cors

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: false,
+  })
+);
 
 app.use('/tasks', taskRoutes);
 
