166d9aea0f2e0b86834b5835ccbc38671357c38b
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-12-19 20:56:21 +0500

    done all  things

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,13 +2,11 @@ const Task = require('../models/Task');
 
 const createTask = async (req, res) => {
   try {
-    const { title, description, status, dueDate } = req.body;
+    const { title, description, status, dueDate } = req.body;  
 
-    // Convert the dueDate to UTC before saving
-    const localDueDate = new Date(dueDate);  // Local date received from the frontend
-    const utcDueDate = new Date(localDueDate.toISOString());  // Convert to UTC
+    const utcDueDate = new Date(dueDate);  
 
-    const newTask = new Task({ title, description, status, dueDate: utcDueDate });
+    const newTask = new Task({ title, description, status, dueDate:utcDueDate });
     const task = await newTask.save();
     res.status(201).json(task);
   } catch (error) {
@@ -19,14 +17,7 @@ const createTask = async (req, res) => {
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find();
-
-    // Convert each task's dueDate from UTC to local time zone
-    const tasksWithLocalDate = tasks.map(task => {
-      task.dueDate = new Date(task.dueDate).toLocaleString();  // Convert to local time
-      return task;
-    });
-
-    res.status(200).json(tasksWithLocalDate);
+    res.status(200).json(tasks);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -37,7 +28,6 @@ const getTaskById = async (req, res) => {
     const task = await Task.findById(req.params.id);
     if (!task) return res.status(404).json({ message: 'Task not found' });
 
-    task.dueDate = new Date(task.dueDate).toLocaleString();
     res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -53,12 +43,7 @@ const updateTask = async (req, res) => {
     task.title = title || task.title;
     task.description = description || task.description;
     task.status = status || task.status;
-
-   
-    if (dueDate) {
-      const localDueDate = new Date(dueDate);  
-      task.dueDate = new Date(localDueDate.toISOString()); 
-    }
+    task.dueDate = dueDate || task.dueDate;
 
     const updatedTask = await task.save();
     res.status(200).json(updatedTask);
